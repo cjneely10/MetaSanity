b46e42adcce0c3ca08ca2aa2e1636f438a7404b1
Christopher Neely <christopher.neely1200@gmail.com>
2019-08-11 13:04:05 -0700

    Remove intermediate files at end of run

--- run_pipedm.py
+++ run_pipedm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import sys
+import shutil
 import argparse
 import subprocess
 from configparser import RawConfigParser
@@ -39,8 +40,6 @@ BIOMETADB = "/path/to/BioMetaDB/dbdm.py"
 SIGNALP_FOLDER = "/path/to/signalp-4.1"
 # RNAmmer software package, including binary, from  http://www.cbs.dtu.dk/cgi-bin/nph-sw_request?rnammer
 RNAMMER_FOLDER = "/path/to/rnammer-1.2.src"
-
-
 
 # BioMetaPipeline version
 DOCKER_IMAGE = "cjneely10/biometapipeline:v0.0.3"
@@ -89,7 +88,7 @@ class ArgParse:
 
 
 class GetDBDMCall:
-    def __init__(self, calling_script_path, db_name, cancel_autocommit, added_flags = []):
+    def __init__(self, calling_script_path, db_name, cancel_autocommit, added_flags=[]):
         """ Class handles determining state of dbdm project
 
         """
@@ -115,11 +114,13 @@ class GetDBDMCall:
             to_run.append("INIT")
             to_run.append("-n")
             to_run.append(self.db_name)
-        elif os.path.exists(self.db_name) and not os.path.exists(os.path.join(self.db_name, "classes", table_name.lower() + ".json")):
+        elif os.path.exists(self.db_name) and not os.path.exists(
+                os.path.join(self.db_name, "classes", table_name.lower() + ".json")):
             to_run.append("CREATE")
             to_run.append("-c")
             to_run.append(self.db_name)
-        elif os.path.exists(self.db_name) and os.path.exists(os.path.join(self.db_name, "classes", table_name.lower() + ".json")):
+        elif os.path.exists(self.db_name) and os.path.exists(
+                os.path.join(self.db_name, "classes", table_name.lower() + ".json")):
             to_run.append("UPDATE")
             to_run.append("-c")
             to_run.append(self.db_name)
@@ -155,35 +156,35 @@ def get_added_flags(config, _dict):
 # Parsed arguments
 ap = ArgParse(
     (
-    (("program",),
-     {"help": "Program to run"}),
-    (("-d", "--directory"),
-     {"help": "Directory containing genomes", "required": True}),
-    (("-c", "--config_file"),
-     {"help": "Config file", "required": True}),
-    (("-a", "--cancel_autocommit"),
-     {"help": "Cancel commit to database", "action": "store_true", "default": False}),
-    (("-o", "--output_directory"),
-     {"help": "Output directory prefix, default out", "default": "out"}),
-    (("-b", "--biometadb_project"),
-     {"help": "/path/to/BioMetaDB_project (updates values of existing database)", "default": "None"}),
-    (("-t", "--type_file"),
-     {"help": "/path/to/type_file formatted as 'file_name.fna\\t[Archaea/Bacteria]\\t[gram+/gram-]\\n'",
-      "default": "None"}),
-    (("-p", "--prokka"),
+        (("program",),
+         {"help": "Program to run"}),
+        (("-d", "--directory"),
+         {"help": "Directory containing genomes", "required": True}),
+        (("-c", "--config_file"),
+         {"help": "Config file", "required": True}),
+        (("-a", "--cancel_autocommit"),
+         {"help": "Cancel commit to database", "action": "store_true", "default": False}),
+        (("-o", "--output_directory"),
+         {"help": "Output directory prefix, default out", "default": "out"}),
+        (("-b", "--biometadb_project"),
+         {"help": "/path/to/BioMetaDB_project (updates values of existing database)", "default": "None"}),
+        (("-t", "--type_file"),
+         {"help": "/path/to/type_file formatted as 'file_name.fna\\t[Archaea/Bacteria]\\t[gram+/gram-]\\n'",
+          "default": "None"}),
+        (("-p", "--prokka"),
          {"help": "Use PROKKA gene calls instead of prodigal search", "default": False, "action": "store_true"}),
     ),
     description=ArgParse.description_builder(
         "pipedm:\tRun meta/genomes evaluation and annotation pipelines",
         {
-            "MET_EVAL":         "Evaluates completion, contamination, and redundancy of MAGs",
-            "MET_ANNOT":        "Runs gene callers and annotation programs on MAGs",
+            "MET_EVAL": "Evaluates completion, contamination, and redundancy of MAGs",
+            "MET_ANNOT": "Runs gene callers and annotation programs on MAGs",
         },
         {
-            "MET_EVAL":       ("directory", "config_file", "cancel_autocommit", "output_directory",
-                               "biometadb_project"),
-            "MET_ANNOT":      ("directory", "config_file", "cancel_autocommit", "output_directory",
-                               "biometadb_project", "type_file"),
+            "MET_EVAL": ("directory", "config_file", "cancel_autocommit", "output_directory",
+                         "biometadb_project"),
+            "MET_ANNOT": ("directory", "config_file", "cancel_autocommit", "output_directory",
+                          "biometadb_project", "type_file"),
         }
     )
 )
@@ -194,8 +195,8 @@ cfg.optionxform = str
 cfg.read(ap.args.config_file)
 
 met_list = {
-    "MET_EVAL":     "metagenome_evaluation.list",
-    "MET_ANNOT":    "metagenome_annotation.list"
+    "MET_EVAL": "metagenome_evaluation.list",
+    "MET_ANNOT": "metagenome_annotation.list"
 }
 
 prokka_add = []
@@ -209,7 +210,7 @@ try:
             "docker",
             "run",
             # user info
-            "--user", 
+            "--user",
             subprocess.getoutput("id -u"),
             # Locale setup required for parsing files
             "-e",
@@ -254,44 +255,48 @@ except KeyboardInterrupt:
     print("\nExiting...")
     sys.exit(1)
 
+out_prefixes = set({})
+
 if not ap.args.cancel_autocommit and os.path.exists(os.path.join(ap.args.output_directory, met_list[ap.args.program])):
     print("\nStoring results to database..........")
     # Primary output file types from MET_ANNOT (with N = number of genomes):
     # Set project name
     try:
-        db_name = (ap.args.biometadb_project 
-            if ap.args.biometadb_project != "None" 
-            else cfg.get("BIOMETADB", "--db_name"))
+        db_name = (ap.args.biometadb_project
+                   if ap.args.biometadb_project != "None"
+                   else cfg.get("BIOMETADB", "--db_name"))
     except:
         db_name = "MetagenomeAnnotation"
 
     dbdm = GetDBDMCall(BIOMETADB, db_name, ap.args.cancel_autocommit, get_added_flags(cfg, "BIOMETADB"))
     if ap.args.program == "MET_ANNOT":
         for _file in (
-            # CAZy (1) - out/peptidase_results/combined_results/combined.cazy
-            os.path.join(ap.args.output_directory, "peptidase_results/combined_results/combined.cazy"),
-            # MEROPS (1) - out/peptidase_results/combined_results/combined.merops
-            os.path.join(ap.args.output_directory, "peptidase_results/combined_results/combined.merops"),
-            # MEROPS pfam (1) - out/peptidase_results/combined_results/combined.merops.pfam
-            os.path.join(ap.args.output_directory, "peptidase_results/combined_results/combined.merops.pfam"),
-            # BioData (1) - out/kegg_results/biodata_results/KEGG.final.tsv
-            os.path.join(ap.args.output_directory, "kegg_results/biodata_results/KEGG.final.tsv"),
-        ):   
+                # CAZy (1) - out/peptidase_results/combined_results/combined.cazy
+                os.path.join(ap.args.output_directory, "peptidase_results/combined_results/combined.cazy"),
+                # MEROPS (1) - out/peptidase_results/combined_results/combined.merops
+                os.path.join(ap.args.output_directory, "peptidase_results/combined_results/combined.merops"),
+                # MEROPS pfam (1) - out/peptidase_results/combined_results/combined.merops.pfam
+                os.path.join(ap.args.output_directory, "peptidase_results/combined_results/combined.merops.pfam"),
+                # BioData (1) - out/kegg_results/biodata_results/KEGG.final.tsv
+                os.path.join(ap.args.output_directory, "kegg_results/biodata_results/KEGG.final.tsv"),
+        ):
             dbdm.run(
-                "functions", 
+                "functions",
                 os.path.join(ap.args.output_directory, "genomes"),
                 _file,
                 "functions",
             )
         # Begin commit individual genomes info
         # Based on file names in metagenome_annotation.list
-        for genome_prefix in (os.path.splitext(os.path.basename(line.rstrip("\r\n")))[0] 
-                                for line in open(os.path.join(ap.args.output_directory, met_list[ap.args.program]))):
+        for genome_prefix in (os.path.splitext(os.path.basename(line.rstrip("\r\n")))[0]
+                              for line in open(os.path.join(ap.args.output_directory, met_list[ap.args.program]))):
             # Virsorter out (N) - out/virsorter_results/*/virsorter-out/*.VIRSorter_adj_out.tsv
+            out_prefixes.add(genome_prefix)
             dbdm.run(
                 genome_prefix.lower(),
                 os.path.join(ap.args.output_directory, "splitfiles", genome_prefix),
-                os.path.join(ap.args.output_directory, "virsorter_results", genome_prefix, "virsorter-out", "%s.VIRSorter_adj_out.tsv" % genome_prefix),
+                os.path.join(ap.args.output_directory, "virsorter_results", genome_prefix, "virsorter-out",
+                             "%s.VIRSorter_adj_out.tsv" % genome_prefix),
                 genome_prefix.lower(),
             )
             # Combined Results (N) - out/*.metagenome_annotation.tsv
@@ -309,3 +314,8 @@ if not ap.args.cancel_autocommit and os.path.exists(os.path.join(ap.args.output_
             "evaluation",
         )
     print("BioMetaDB project complete!")
+
+for prefix in out_prefixes:
+    os.remove(os.path.join(ap.args.output_directory, prefix + ".metagenome_annotation_tmp.tsv"))
+shutil.rmtree(os.path.join(ap.args.output_directory, "genomes"))
+shutil.rmtree(os.path.join(ap.args.output_directory, "splitfiles"))
