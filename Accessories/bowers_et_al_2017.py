81a307ff65d2d9879806d3d0c0a4b0b9b00ed5ab
Christopher Neely <christopher.neely1200@gmail.com>
2019-10-13 19:41:34 -0700

    Draft bowers update

--- Accessories/bowers_et_al_2017.py
+++ Accessories/bowers_et_al_2017.py
@@ -5,13 +5,12 @@ from BioMetaDB import get_table, genomes
 
 assert len(sys.argv) == 2, "usage: python3 bowers_et_al_2017.py <biometadb-project>"
 
-genomes = genomes(sys.argv[1])
-
-evaluation_data = get_table(sys.argv[1], "evaluation")
-evaluation_data.query()
-
 output_filename = "bowers-quality.tsv"
 output_file = open(output_filename, "w")
+
+genomes = genomes(sys.argv[1])
+evaluation_data = get_table(sys.argv[1], "evaluation")
+evaluation_data.query()
 
 output_file.write("ID\tquality\n")
 for genome in genomes:
@@ -19,8 +18,8 @@ for genome in genomes:
     num_tRNAs = len(genome_rl.query("(prokka LIKE '%tRNA%) '"))
     has_23_16_rRNA = genome_rl.query("prokka LIKE '%23S%'") and genome_rl.query("prokka LIKE '%16S%'")
     completion = evaluation_data[genome].completion
-    contamination = evaluation_data[genome.contamination]
-    # High quality
+    contamination = evaluation_data[genome].contamination
+    # Bowers et al determinations for MAG/SAG assembly quality
     if num_tRNAs >= 18 and has_23_16_rRNA and completion > 90 and contamination < 5:
         output_file.write(genome + "\thigh\n")
     elif completion >= 50 and contamination < 10:
