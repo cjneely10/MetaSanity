e00fba5a901a2bd2db547d7238088745e1eabf0d
Christopher Neely <christopher.neely1200@gmail.com>
2019-12-24 23:55:03 -0800

    FastaParser via Rust ready for DataFrame implementation

---
+++ MetaSanity/Parsers/fasta_ops/src/fasta_parser.rs
@@ -0,0 +1,114 @@
+use std::io::{BufRead, BufReader};
+
+pub struct FastaParser {
+    fasta_file: String,
+    file_header: Option<String>,
+    record_locations: Option<std::collections::HashMap<String, String>>
+}
+
+impl FastaParser {
+    pub fn parse_to_std(_file: &str, header_as_id: bool) {
+        let mut file_header: Option<String> = None;
+        if header_as_id {
+            file_header = Some(
+                String::from(
+                    std::path::Path::new(&_file)
+                        .file_stem()
+                        .and_then(std::ffi::OsStr::to_str)
+                        .unwrap()
+                )
+            );
+        }
+        let fp = FastaParser{
+            fasta_file: _file.to_string(), 
+            file_header: file_header,
+            record_locations: None
+        };
+        fp.write_simple();
+    }
+
+    pub fn new(_file: &str) {
+
+    }
+
+    fn write_simple(&self) {
+        // File reader variables for parsing into 80 char chunks
+        let mut line_loc: usize = 0;
+        let mut end_of_line: bool = false;
+        let mut counter: u16 = 0;
+        let reader = BufReader::new(std::fs::File::open(self.fasta_file.clone()).unwrap());
+
+        // Parse line by line
+        for line in reader.lines() {
+            let line = line.expect("Unable to read line");
+            let _line = line.as_bytes();
+            if _line.len() > 0 {
+                // Based on first character of each line
+                match _line[0] {
+                    // Write fasta header
+                    b'>' => {
+                        // Based on name of file
+                        match self.file_header.as_ref() {
+                            None => {
+                                self.write_header(&_line);
+                            }
+                            Some(header) => {
+                                println!(">{}_{}", header, counter);
+                                counter += 1;
+                            }
+                        }
+                    },
+                    // Write line in 80 character segments
+                    _ => self.print_line_to_80(&_line, &mut line_loc, &mut end_of_line)
+                };
+            }
+        }
+    }
+
+    /// Calls print for 80 char line segments
+    /// Builds passed buffer and writes once it is 80 chars long
+    fn print_line_to_80(&self, line: &[u8], line_loc: &mut usize, end_of_line: &mut bool) {
+        for (i, &item) in line.iter().enumerate() {
+            if item != b'\n' {
+                // Record to write and line available
+                if *line_loc < 80 {         
+                    print!("{}", item as char);
+                    *line_loc += 1;
+                }
+                // Record to write, line unavailable
+                else {
+                    print!("\n");
+                    *line_loc = 0;
+                    // Recursive call for remaining length of line
+                    self.print_line_to_80(&line[i..], line_loc, end_of_line);
+                    break;
+                }
+            }
+        }
+        // Print end of line
+        print!("\n");
+        // Update location in line to be at beginning of buffer
+        *line_loc = 0;
+    }
+    
+    /// Returns up to the first space in the header
+    fn write_header<'a>(&self, line: &'a [u8]) {
+        let mut line_len = line.len();
+        for (i, &item) in line.iter().enumerate() {
+            // Identify location of space, if present, and break
+            if item == b' ' {
+                line_len = i;
+                break;
+            }
+        }
+        // Print up to space, or length of line, if short enough
+        if line_len <= 20 { 
+            println!("{}", std::str::from_utf8(&line[0..line_len]).unwrap());
+        }
+        // Print only first 12 and last 8 chars of line for max 20
+        else { 
+            print!("{}", std::str::from_utf8(&line[..12]).unwrap());
+            println!("{}", std::str::from_utf8(&line[line_len - 9..]).unwrap());
+        }
+    }
+}
--- MetaSanity/Parsers/fasta_ops/src/main.rs
+++ MetaSanity/Parsers/fasta_ops/src/main.rs
@@ -1,10 +1,11 @@
-use std::io::{BufRead, BufReader};
+// use std::io::{BufRead, BufReader};
 extern crate argparse;
-use argparse::{ArgumentParser, Store};
+use argparse::{ArgumentParser, Store, StoreTrue};
+mod fasta_parser;
 
 fn main() -> std::io::Result<()> {
     let mut fasta_file = String::new();
-    let mut header_adj = "record".to_string();
+    let mut header_adj = false;
     
     // Parse arguments using scoped borrows
     {
@@ -14,102 +15,106 @@ fn main() -> std::io::Result<()> {
                 .add_argument("fasta-file", Store, "Fasta file to modify")
                 .required();
         parser.refer(&mut header_adj)
-                .add_option(&["-t", "--truncate-type"], Store, 
-                    "Determine by file/record, default record");
+                .add_option(&["-n", "--name-by-file"], StoreTrue, 
+                    "Name by file instead of default name by record");
         parser.parse_args_or_exit();
     }
 
     // Confirm that header replace type is valid
-    let possible_types = ["file".to_string(), "record".to_string()];
-    if !possible_types.contains(&header_adj) { 
-        println!("Incorrect header parse type");
-        std::process::exit(1);
-    }
+    // let possible_types = ["file".to_string(), "record".to_string()];
+    // if !possible_types.contains(&header_adj) { 
+    //     println!("Incorrect header parse type");
+    //     std::process::exit(1);
+    // }
 
-    // Determine file basename if needed
-    // Provide counter for indexing file
-    let ff = fasta_file.clone();
-    let mut counter = 0;
-    let mut file_header : Option<&str> = None;
-    if header_adj == "file" { 
-        file_header = std::path::Path::new(&ff).file_stem().and_then(std::ffi::OsStr::to_str);
-    }
+    fasta_parser::FastaParser::parse_to_std(&fasta_file, header_adj);
 
-    // File reader variables for parsing into 80 char chunks
-    let mut line_loc: usize = 0;
-    let mut end_of_line: bool = false;
-    let reader = BufReader::new(std::fs::File::open(fasta_file).unwrap());
+    // // Determine file basename if needed
+    // // Provide counter for indexing file
+    // let ff = fasta_file.clone();
+    // let mut counter = 0;
+    // let mut file_header : Option<&str> = None;
+    // if header_adj == "file" { 
+    //     file_header = std::path::Path::new(&ff).file_stem().and_then(std::ffi::OsStr::to_str);
+    // }
 
-    // Parse line by line
-    for line in reader.lines() {
-        let line = line.expect("Unable to read line");
-        let _line = line.as_bytes();
-        if _line.len() > 0 {
-            // Based on first character of each line
-            match _line[0] {
-                // Write fasta header
-                b'>' => {
-                    // Based on name of file
-                    if file_header != None {
-                        println!(">{}_{}", file_header.unwrap(), counter);
-                        counter += 1;
-                    }
-                    // Based on record
-                    else {
-                        write_header(&_line);
-                    }
-                },
-                // Write line in 80 character segments
-                _ => print_line_to_80(&_line, &mut line_loc, &mut end_of_line)
-            };
-        }
-    }
+    // // let fp = fasta_parser::FastaParser::new(fasta_file, file_header);
+
+    // // File reader variables for parsing into 80 char chunks
+    // let mut line_loc: usize = 0;
+    // let mut end_of_line: bool = false;
+    // let reader = BufReader::new(std::fs::File::open(fasta_file).unwrap());
+
+    // // Parse line by line
+    // for line in reader.lines() {
+    //     let line = line.expect("Unable to read line");
+    //     let _line = line.as_bytes();
+    //     if _line.len() > 0 {
+    //         // Based on first character of each line
+    //         match _line[0] {
+    //             // Write fasta header
+    //             b'>' => {
+    //                 // Based on name of file
+    //                 if file_header != None {
+    //                     println!(">{}_{}", file_header.unwrap(), counter);
+    //                     counter += 1;
+    //                 }
+    //                 // Based on record
+    //                 else {
+    //                     write_header(&_line);
+    //                 }
+    //             },
+    //             // Write line in 80 character segments
+    //             _ => print_line_to_80(&_line, &mut line_loc, &mut end_of_line)
+    //         };
+    //     }
+    // }
     Ok(())
 }
 
-/// Calls print for 80 char line segments
-/// Builds passed buffer and writes once it is 80 chars long
-fn print_line_to_80(line: &[u8], line_loc: &mut usize, end_of_line: &mut bool) {
-    for (i, &item) in line.iter().enumerate() {
-        if item != b'\n' {
-            // Record to write and line available
-            if *line_loc < 80 {         
-                print!("{}", item as char);
-                *line_loc += 1;
-            }
-            // Record to write, line unavailable
-            else {
-                print!("\n");
-                *line_loc = 0;
-                // Recursive call for remaining length of line
-                print_line_to_80(&line[i..], line_loc, end_of_line);
-                break;
-            }
-        }
-    }
-    // Print end of line
-    print!("\n");
-    // Update location in line to be at beginning of buffer
-    *line_loc = 0;
-}
+// /// Calls print for 80 char line segments
+// /// Builds passed buffer and writes once it is 80 chars long
+// fn print_line_to_80(line: &[u8], line_loc: &mut usize, end_of_line: &mut bool) {
+//     for (i, &item) in line.iter().enumerate() {
+//         if item != b'\n' {
+//             // Record to write and line available
+//             if *line_loc < 80 {         
+//                 print!("{}", item as char);
+//                 *line_loc += 1;
+//             }
+//             // Record to write, line unavailable
+//             else {
+//                 print!("\n");
+//                 *line_loc = 0;
+//                 // Recursive call for remaining length of line
+//                 print_line_to_80(&line[i..], line_loc, end_of_line);
+//                 break;
+//             }
+//         }
+//     }
+//     // Print end of line
+//     print!("\n");
+//     // Update location in line to be at beginning of buffer
+//     *line_loc = 0;
+// }
 
-/// Returns up to the first space in the header
-fn write_header<'a>(line: &'a [u8]) {
-    let mut line_len = line.len();
-    for (i, &item) in line.iter().enumerate() {
-        // Identify location of space, if present, and break
-        if item == b' ' {
-            line_len = i;
-            break;
-        }
-    }
-    // Print up to space, or length of line, if short enough
-    if line_len <= 20 { 
-        println!("{}", std::str::from_utf8(&line[0..line_len]).unwrap());
-    }
-    // Print only first 12 and last 8 chars of line for max 20
-    else { 
-        print!("{}", std::str::from_utf8(&line[..12]).unwrap());
-        println!("{}", std::str::from_utf8(&line[line_len - 9..]).unwrap());
-    }
-}
+// /// Returns up to the first space in the header
+// fn write_header<'a>(line: &'a [u8]) {
+//     let mut line_len = line.len();
+//     for (i, &item) in line.iter().enumerate() {
+//         // Identify location of space, if present, and break
+//         if item == b' ' {
+//             line_len = i;
+//             break;
+//         }
+//     }
+//     // Print up to space, or length of line, if short enough
+//     if line_len <= 20 { 
+//         println!("{}", std::str::from_utf8(&line[0..line_len]).unwrap());
+//     }
+//     // Print only first 12 and last 8 chars of line for max 20
+//     else { 
+//         print!("{}", std::str::from_utf8(&line[..12]).unwrap());
+//         println!("{}", std::str::from_utf8(&line[line_len - 9..]).unwrap());
+//     }
+// }
